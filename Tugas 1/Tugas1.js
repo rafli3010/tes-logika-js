06b3c00e30cc0899d407d368172cd7009a442801
Rafli Rakha Pratama <65547086+rafli3010@users.noreply.github.com>
2024-01-16 20:26:02 +0700

    Tugas1.js

--- Tugas 1/Tugas1.js
+++ Tugas 1/Tugas1.js
@@ -15,7 +15,5 @@ function hitungKaosKaki(arr) {
 }
 
 console.log("Output : " + hitungKaosKaki([10, 20, 20, 10, 10, 30, 50, 10, 20])); // Output: 3
-console.log(
-  "Output : " + hitungKaosKaki([6, 5, 2, 3, 5, 2, 2, 1, 1, 5, 1, 3, 3, 3, 5])
-); // Output: 6
+console.log("Output : " + hitungKaosKaki([6, 5, 2, 3, 5, 2, 2, 1, 1, 5, 1, 3, 3, 3, 5])); // Output: 6
 console.log("Output : " + hitungKaosKaki([1, 1, 3, 1, 2, 1, 3, 3, 3, 3])); // Output: 4
